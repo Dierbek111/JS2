b9c9071c76fa5f35a201de191772436df30419ea
Dierbek111 <dierbekrahmatullaev00@gmail.com>
2024-07-27 11:20:00 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+let a = prompt('Введите любое число')
+
+if (a < 0) {
+    console.log('отрицательное число');
+}
+else if (a > 0) {
+    console.log('положительное число');
+}
+else { console.log('вы ввели ноль'); }
